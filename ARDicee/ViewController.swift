b7709b367b04f233ba1f2a25d2a5539227aa99a0
leewanjae <dhkswo1@gmail.com>
2024-03-17 22:36:06 +0900

    feat: 터치한 평면 위치에 주사위 생성

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -10,7 +10,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -20,15 +20,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
-       
+        
         sceneView.automaticallyUpdatesLighting = true
         
-//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-//        // 재순환적으로 노드에 있는 모든 서브트리를 포함시켜 트리를 검색할 수 있게 해준다.
-//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-//            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-//            sceneView.scene.rootNode.addChildNode(diceNode)
-//        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -49,6 +43,33 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    // 뷰나 윈도우에서 사용자로부터 오는 터치가 감지되면 호출된다
+    // 가장 먼저 정말로 터치 여부를 확인해야함.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            
+            // ipone의 x,y를 기반으로 z축 산출?
+            // 터치한 실제 위치와 동일하게 해줌
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = results.first {
+                print(hitResult)
+                
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+                // 재순환적으로 노드에 있는 모든 서브트리를 포함시켜 트리를 검색할 수 있게 해준다.
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    diceNode.position = SCNVector3(
+                        x: hitResult.worldTransform.columns.3.x,
+                        y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius, // 단순 3.y만 설정시 그리드와 겹치게 주사위가 생김 따라서 너비의 반경을 더해줌
+                        z: hitResult.worldTransform.columns.3.z
+                    )
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+                }
+            }
+        }
     }
     
     // 새로운 수평면을 감지하면 먼저 이 메서드를 호출하고 안에있는 코드를 트리거
